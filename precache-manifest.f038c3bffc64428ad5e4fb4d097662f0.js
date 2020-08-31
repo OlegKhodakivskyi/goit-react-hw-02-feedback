ca208ca4aee7d7185a18738e2d93ffeb43861fe2
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-08-31 16:21:32 +0300

    Updates

---
+++ precache-manifest.f038c3bffc64428ad5e4fb4d097662f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28efc478dca9bb266d50029b0b9343dd",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "dfecf363b9111312fdaa",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.18cad9f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "507c2f5710d6b67a81f8",
+    "url": "/goit-react-hw-02-feedback/static/js/main.454c9565.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
